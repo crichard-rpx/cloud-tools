998f89e233eca188ddbd29abae63ef795784778b
Chris Richard <crichard@rpxcorp.com>
2017-06-01 11:24:00 -0700

    starting node sync tool using python

---
+++ python/sky-sync.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import boto3
+import subprocess
+
+ec = boto3.resource('ec2')
+
+instances = ec.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+aws_nodes = []
+
+for instance in instances:
+    hostname = [tag for tag in instance.tags if tag['Key'] == 'Name']
+    aws_nodes.append(hostname[0]['Value'].split()[0])
+
+print(len(aws_nodes))
+#print(aws_nodes)
+
+chef_nodes = subprocess.check_output(["knife", "node", "list"]).split()
+print(len(chef_nodes))
+
+
+
